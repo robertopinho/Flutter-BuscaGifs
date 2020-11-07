e28c1b2bd683e7d16809f5ca87eaf2fc6ccab947
Roberto Pinho <robertoopinho@gmail.com>
2020-11-07 17:38:40 -0300

    Enviando APP

---
+++ android/app/src/main/java/com/example/busca_gif/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.busca_gif;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:busca_gif/ui/gif_page.dart';
+import 'package:flutter/material.dart';
+import 'package:busca_gif/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
